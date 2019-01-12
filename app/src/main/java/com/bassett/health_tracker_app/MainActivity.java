0b5469c3ad1006538f768c26de5c880ac08ecd48
mason bassett <bassettmason@gmail.com>
2019-01-12 14:35:06 -0800

    notify not working

--- app/src/main/java/com/bassett/health_tracker_app/MainActivity.java
+++ app/src/main/java/com/bassett/health_tracker_app/MainActivity.java
@@ -1,8 +1,11 @@
 package com.bassett.health_tracker_app;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
-import android.os.Handler;
-import android.os.SystemClock;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,10 +13,12 @@ import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String CHANNEL_ID = "channelId";
     public static final String life = "hello world";
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+    }
+
+    public void onNotificationClick(View v){
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notificationbell)
+                .setContentTitle("Title")
+                .setContentText("text")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("bigtext"))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(1,mBuilder.build());
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Notifications";
+            String description = "notifications stuff";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     public void sendToInspiration(View v) {
