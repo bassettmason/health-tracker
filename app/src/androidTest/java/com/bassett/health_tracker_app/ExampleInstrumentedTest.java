2f37edc1c2d7cb9c6bacf15d8014eaa177ac5f5a
mason bassett <bassettmason@gmail.com>
2019-01-20 08:57:10 -0800

    progress

--- app/src/androidTest/java/com/bassett/health_tracker_app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/bassett/health_tracker_app/ExampleInstrumentedTest.java
@@ -18,7 +18,6 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.bassett.health_tracker_app", appContext.getPackageName());
